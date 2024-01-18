497112330ca42fe8100ba8e44dfe1acafd97831d
Anrakkii <aLoveanime9x@gmail.com>
2024-01-18 17:03:32 +0700

    update index

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/00e90bdb6c.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./style.css">
-    <title>Project</title>
-</head>
-<body>
+
     <?php
         include("./main/header.php");
         include("./main/slider.php");
         include("./main/app_container.php");
         include("./main/footer.php");
     ?>
-</body>
-<script src="./style.js"></script>
-</html>
